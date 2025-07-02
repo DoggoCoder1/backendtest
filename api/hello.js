3cc7cd1263cebe313703b1e139c644d57d67f6e1
wowdog15 <MaddoxVarji@Lizs-iMac.local>
2025-07-01 21:38:07 -0400

    wowdog15

--- api/hello.js
+++ api/hello.js
@@ -6,6 +6,10 @@ const pool = new Pool({
 });
 
 export default async function handler(req, res) {
+  if (process.env.maintmode === 'true') {
+  return res.status(503).json({ message: 'disabled' });
+  }
+
   const ip = req.headers['x-forwarded-for']?.split(',')[0] || req.socket.remoteAddress;
   let client;
 

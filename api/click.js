7cc763e7204b2eb1cfe5821b9f879c6025fc336f
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-07-06 18:20:07 -0400

    wowdog15

--- api/click.js
+++ api/click.js
@@ -1,4 +1,4 @@
-import { Pool } from 'pg';
+/*import { Pool } from 'pg';
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
@@ -124,3 +124,4 @@ export default async function handler(req, res) {
     client.release();
   }
 }
+*/

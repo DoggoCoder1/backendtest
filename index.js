e56224aaa3a3b77fea8d570b2fd9019e1e2c23db
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-20 11:29:40 -0400

    New

---
+++ index.js
@@ -0,0 +1,25 @@
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.GMAIL_USER,
+    pass: process.env.GMAIL_PASS
+  }
+});
+
+const mailOptions = {
+  from: process.env.GMAIL_USER,
+  to: 'pumpkinsarecool899@gmail.com',
+  subject: 'Test Email from Nodemailer',
+  text: 'Hello, this is a test email sent using Nodemailer! If you received this, it worked.'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+  if (error) {
+    console.error('Error sending email:', error);
+  } else {
+    console.log('Email sent:', info.response);
+  }
+});

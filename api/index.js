138960a38212b3a382a9e86aeecfdab63db38f1b
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-20 11:54:54 -0400

    New

--- api/index.js
+++ api/index.js
@@ -1,17 +1,9 @@
-// server.js
-const express = require('express');
-const app = express();
-
-app.use(express.json());
-
-// if you want to POST:
-app.post('/api/index', (req, res) => {
-  res.json({ message: 'POST request received', body: req.body });
-});
-
-// if you also want GET:
-app.get('/api/index', (req, res) => {
-  res.json({ message: 'GET request received' });
-});
-
-app.listen(3000, () => console.log('Server running on port 3000'));
+export default function handler(req, res) {
+  if (req.method === 'POST') {
+    res.status(200).json({ message: 'POST request received', body: req.body });
+  } else if (req.method === 'GET') {
+    res.status(200).json({ message: 'GET request received' });
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}

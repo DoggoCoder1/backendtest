07f9875abb8d7bdc0ae38908bd4faf2bbed86b70
wowdog15 <Pumpkinsarecool899@gmail.com>
2025-08-20 11:57:34 -0400

    New

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 export default function handler(req, res) {
   if (req.method === 'POST') {
-    res.status(200).json({ message: 'POST request received', body: req.body });
-  } else if (req.method === 'GET') {
-    res.status(200).json({ message: 'GET request received' });
-  } else {
-    res.status(405).json({ error: 'Method not allowed' });
+    res.send("aaa");
   }
 }
